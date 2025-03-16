e39df39063039314f66be33b312b66cabc802041
Santhanamsk02 <santhanakrishnan9360@gmail.com>
2025-03-16 13:46:21 +0530

    Change first page to login.html

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  });
 app.use(express.static(path.join(__dirname, 'public'))); // Serve static files
+
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb+srv://chandru127001:hfjSd3QoX5y6Gn6I@aiclassroom.hrciz.mongodb.net/?retryWrites=true&w=majority&appName=AIclassroom";
 const DB_NAME = "mydatabase";
@@ -75,9 +79,7 @@ const uploadToGridFS = async (fileBuffer, filename, fileType, metadata = {}) => 
   }
 };
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
-});
+
 
 app.post('/signup', async (req, res) => {
   const { fullName, subject, email, password, Class } = req.body;
